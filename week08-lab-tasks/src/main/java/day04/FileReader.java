5b49475a17f95d6865abb3a41dd0e12ef4510d76
he <bgyhenrietta@yahoo.com>
2021-12-03 16:15:38 +0100

    week08 day04 fileReader

--- week08-lab-tasks/src/main/java/day04/FileReader.java
+++ week08-lab-tasks/src/main/java/day04/FileReader.java
@@ -2,15 +2,16 @@ package day04;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
 public class FileReader {
 
     public static void main(String[] args) {
-        int min = new FileReader().findSmallestTemperatureSpread("weather.dat");
-        System.out.println(min);
+        int minTemperature = new FileReader().findSmallestTemperatureSpread("weather.dat");
+        System.out.println(minTemperature);
+        String name = new FileReader().findSmallestTeamDifference("football.dat");
+        System.out.println(name);
     }
 
     public int findSmallestTemperatureSpread(String filename) {
@@ -32,6 +33,32 @@ public class FileReader {
                 }
             }
             return minDay;
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not read file!", ioe);
+        }
+    }
+
+    public String findSmallestTeamDifference(String filename) {
+        try {
+            List<String> lines = Files.readAllLines(Paths.get("src/main/resources/football.dat"));
+
+            int minDifference = 1000;
+            String minName = null;
+
+            for (int i = 1; i < lines.size(); i++) {
+                if (i != 18) {
+                    String name = lines.get(i).substring(7, 22).trim();
+                    int max = Integer.parseInt(lines.get(i).substring(43, 45).trim());
+                    int min = Integer.parseInt(lines.get(i).substring(50, 52).trim());
+
+                    int spreed = Math.abs(max - min);
+                    if (minDifference > spreed) {
+                        minDifference = spreed;
+                        minName = name;
+                    }
+                }
+            }
+            return minName;
         } catch (IOException ioe) {
             throw new IllegalStateException("Can not read file!", ioe);
         }
